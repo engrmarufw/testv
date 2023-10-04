fb86b70600516fcd35a2badf4ef03dfb5309dc6c
engrmarufw <engrmarufw@gmail.com>
2023-10-04 13:02:41 +0600

    update

--- index.js
+++ index.js
@@ -26,13 +26,13 @@ const client = new MongoClient(uri, {
 
 const verifyJWT = (req, res, next) => {
     const authorization = req.headers.authorization;
-    if(!authorization){
-        return res.status(401).send({error: true, message: 'unauthorized access'});
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' });
     }
     const token = authorization.split(' ')[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
-        if(err){
-            return res.status(401).send({error: true, message: 'unauthorized access'})
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
         }
         req.decoded = decoded;
         next();
@@ -52,15 +52,15 @@ async function run() {
         app.get('/allusers', async (req, res) => {
             const result = await usersCollection.find().toArray();
             res.send(result);
-          });
-      
+        });
+
         // jwt
         app.post('/jwt', (req, res) => {
             const user = req.body;
             console.log(user);
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
             console.log(token);
-            res.send({token});
+            res.send({ token });
         })
 
         // services routes
@@ -89,8 +89,8 @@ async function run() {
             const decoded = req.decoded;
             console.log('came back after verify', decoded)
 
-            if(decoded.email !== req.query.email){
-                return res.status(403).send({error: 1, message: 'forbidden access'})
+            if (decoded.email !== req.query.email) {
+                return res.status(403).send({ error: 1, message: 'forbidden access' })
             }
 
             let query = {};
