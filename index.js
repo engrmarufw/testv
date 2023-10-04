b2e7d7653d58ffc7873f3126a32a465d9681c34e
engrmarufw <engrmarufw@gmail.com>
2023-10-04 13:11:16 +0600

    update

--- index.js
+++ index.js
@@ -5,13 +5,13 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
+const bcrypt = require('bcrypt');
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
 
-console.log(process.env.DB_PASS)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xhirfgb.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -53,81 +53,71 @@ async function run() {
             const result = await usersCollection.find().toArray();
             res.send(result);
         });
+        app.post("/login-user", async (req, res) => {
+            const { email, password, roll } = req.body;
 
-        // jwt
-        app.post('/jwt', (req, res) => {
-            const user = req.body;
-            console.log(user);
-            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
-            console.log(token);
-            res.send({ token });
-        })
+            const admin = await usersCollection.findOne({ $and: [{ email: email }, { role: "admin" }] });
+            const teacher = await teacherCollection.findOne({ $and: [{ email: email }, { role: "teacher" }] });
+            const student = await studentCollection.findOne({ $and: [{ email: email }, { role: "student" }] });
+            const studentid = await studentCollection.findOne({ $and: [{ roll: roll }, { role: "student" }] });
 
-        // services routes
-        app.get('/services', async (req, res) => {
-            const cursor = serviceCollection.find();
-            const result = await cursor.toArray();
-            res.send(result);
-        })
+            if (admin) {
+                if (await bcrypt.compare(password, admin.password)) {
+                    const token = jwt.sign({ email: admin.email }, process.env.ACCESS_TOKEN_SECRET, {
+                        expiresIn: "1h",
+                    });
 
-        app.get('/services/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-
-            const options = {
-                // Include only the `title` and `imdb` fields in the returned document
-                projection: { title: 1, price: 1, service_id: 1, img: 1 },
-            };
-
-            const result = await serviceCollection.findOne(query, options);
-            res.send(result);
-        })
-
-
-        // bookings routes
-        app.get('/bookings', verifyJWT, async (req, res) => {
-            const decoded = req.decoded;
-            console.log('came back after verify', decoded)
-
-            if (decoded.email !== req.query.email) {
-                return res.status(403).send({ error: 1, message: 'forbidden access' })
+                    if (res.status(201)) {
+                        return res.json({ status: "ok", data: token });
+                    } else {
+                        return res.json({ error: "error" });
+                    }
+                }
             }
+            if (teacher) {
+                if (await bcrypt.compare(password, teacher.password)) {
+                    const token = jwt.sign({ email: teacher.email }, process.env.ACCESS_TOKEN_SECRET, {
+                        expiresIn: "1h",
+                    });
 
-            let query = {};
-            if (req.query?.email) {
-                query = { email: req.query.email }
+                    if (res.status(201)) {
+                        return res.json({ status: "ok", data: token });
+                    } else {
+                        return res.json({ error: "error" });
+                    }
+                }
             }
-            const result = await bookingCollection.find(query).toArray();
-            res.send(result);
-        })
+            if (student) {
+                if (await bcrypt.compare(password, student.password)) {
+                    const token = jwt.sign({ email: student.email }, process.env.ACCESS_TOKEN_SECRET, {
+                        expiresIn: "1h",
+                    });
 
-        app.post('/bookings', async (req, res) => {
-            const booking = req.body;
-            console.log(booking);
-            const result = await bookingCollection.insertOne(booking);
-            res.send(result);
+                    if (res.status(201)) {
+                        return res.json({ status: "ok", data: token });
+                    } else {
+                        return res.json({ error: "error" });
+                    }
+                }
+            }
+            if (studentid) {
+                if (await bcrypt.compare(password, studentid.password)) {
+                    const token = jwt.sign({ roll: studentid.roll }, process.env.ACCESS_TOKEN_SECRET, {
+                        expiresIn: "1h",
+                    });
+
+                    if (res.status(201)) {
+                        return res.json({ status: "ok", data: token });
+                    } else {
+                        return res.json({ error: "error" });
+                    }
+                }
+            }
+            else {
+                return res.json({ error: "User Not found" });
+            }
+            res.json({ status: "error", error: "InvAlid Password" });
         });
-
-        app.patch('/bookings/:id', async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: new ObjectId(id) };
-            const updatedBooking = req.body;
-            console.log(updatedBooking);
-            const updateDoc = {
-                $set: {
-                    status: updatedBooking.status
-                },
-            };
-            const result = await bookingCollection.updateOne(filter, updateDoc);
-            res.send(result);
-        })
-
-        app.delete('/bookings/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: new ObjectId(id) }
-            const result = await bookingCollection.deleteOne(query);
-            res.send(result);
-        })
 
 
         // Send a ping to confirm a successful connection
